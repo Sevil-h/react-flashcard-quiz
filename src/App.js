84522bf760835c284b0e7ef09267233638e52a5d
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-06-07 20:46:21 +0300

    added fleshcards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import FlashcardList from "./components/FlashcardList";
 import "./App.css";
 
+const SAMPLE_FLASHCARD = [
+	{
+		id: 1,
+		question: "What is 2 + 2?",
+		answer: "4",
+		options: ["2", "3", "4", "5"],
+	},
+	{
+		id: 2,
+		question: "Question",
+		answer: "Answer",
+		options: ["Answer1", "Answer2", "Answer3", "Answer4"],
+	},
+];
+
 function App() {
-	return <h1>Hello World</h1>;
+	const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARD);
+	return <FlashcardList flashcards={flashcards} />;
 }
 
 export default App;
