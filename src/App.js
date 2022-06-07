9d3c6b9603f5ad6bdf52dd4239accce9f51d5c43
Sevil Hatipoglu <sevilhatipogluu93@gmail.com>
2022-06-07 22:03:48 +0300

    fetch db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FlashcardList from "./components/FlashcardList";
 import "./App.css";
+import axios from "axios";
 
 const SAMPLE_FLASHCARD = [
 	{
@@ -18,6 +19,32 @@ const SAMPLE_FLASHCARD = [
 ];
 
 function App() {
+	useEffect(() => {
+		axios.get(`https://opentdb.com/api.php?amount=10`).then((res) => {
+			setFlashcards(
+				res.data.results.map((questionItem, index) => {
+					const answer = decodeString(questionItem.correct_answer);
+					const options = [
+						...questionItem.incorrect_answers.map((a) => decodeString(a)),
+						answer,
+					];
+					return {
+						id: `${index}-${Date.now()}`,
+						question: decodeString(questionItem.question),
+						answer: answer,
+						options: options.sort(() => Math.random() - 0.5),
+					};
+				})
+			);
+		});
+	}, []);
+
+	function decodeString(str) {
+		const textArea = document.createElement("textarea");
+		textArea.innerHTML = str;
+		return textArea.value;
+	}
+
 	const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARD);
 	return <FlashcardList flashcards={flashcards} />;
 }
